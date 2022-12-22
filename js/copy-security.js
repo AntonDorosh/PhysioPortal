6a110f9f12c12ad4b846f251dd9df85de4e8abf7
Anton Dorosh <antondrsh@gmail.com>
2022-12-22 21:29:48 +0200

    add js files

--- js/copy-security.js
+++ js/copy-security.js
@@ -1,13 +1,13 @@
-// добавляет источник при копировании информации
+// добавляет источник при копировании текста
 function PhysioPortalLink() {
-  var istS = 'Источник:';
-  var copyR = '© PhysioPortal - портал про фізичну реабілітацію після травм, операцій та захворювань';
-  var body_element = document.getElementsByTagName('body')[0];
-  var choose = window.getSelection();
-  var myLink = document.location.href;
-  var authorLink = "<br /><br />" + istS + ' ' + "<a href='"+myLink+"'>"+myLink+"</a><br />" + copyR;
-  var copytext = choose + authorLink;
-  var addDiv = document.createElement('div');
+  const istS = 'Источник:';
+  const copyR = '© PhysioPortal - портал про фізичну реабілітацію після травм, операцій та захворювань';
+  const body_element = document.getElementsByTagName('body')[0];
+  const choose = window.getSelection();
+  const myLink = document.location.href;
+  const authorLink = "<br /><br />" + istS + ' ' + "<a href='"+myLink+"'>"+myLink+"</a><br />" + copyR;
+  const copytext = choose + authorLink;
+  const addDiv = document.createElement('div');
   addDiv.style.position='absolute';
   addDiv.style.left='-99999px';
   body_element.appendChild(addDiv);
