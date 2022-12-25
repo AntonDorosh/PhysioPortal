48292906fda18b8840c9ec78109acfcbe015004b
Anton Dorosh <antondrsh@gmail.com>
2022-12-25 13:02:27 +0200

    add footer

--- js/aside.js
+++ js/aside.js
@@ -1,18 +1,4 @@
-// // // Выпадающий список боковой навигации
-// const dropdown = document.getElementsByClassName('dropdown__btn-sidebar');
-
-// for (let i = 0; i < dropdown.length; i++) {
-//   dropdown[i].addEventListener('click', function() {
-//     this.classList.toggle('active');
-//     const dropdownContent = this.nextElementSibling;
-//     if (dropdownContent.style.display === 'block') {
-//       dropdownContent.style.display = 'none';
-//     } else {
-//       dropdownContent.style.display = 'block';
-//     }
-//   })
-// }
-
+// выпадающий список боковой навигации
 const acc = document.getElementsByClassName("dropdown__btn-sidebar");
 let i;
 
@@ -27,6 +13,10 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// фиксация сайдбара при прокрутке страницы
+
+
 
 
 
